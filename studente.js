2899b7a8433e39077fded6e7c1ed6429b5aee5b1
carminenicoletta91 <carminenicolettakr91@gmail.com>
2019-07-10 17:51:47 +0200

    --completato punto 3 ma voglio migliorarlo--

--- studente.js
+++ studente.js
@@ -13,28 +13,41 @@ for (var x in studente) {
 Ciclare su tutti gli studenti e stampare per ognuno nome e cognome*/
 var studenti =[
   {
-    label:"studente1",
+
     nome:"mario",
     cognome:"rossi",
     eta:20
   },
   {
-    label:"studente2",
+
     nome:"filippo",
     cognome:"inzaghi",
     eta:40
   },
   {
-    label:"studente3",
+
     nome:"cesare",
     cognome:"cremonini",
     eta:35
   }
 
 ];
+console.log(studenti);
 for(var i = 0; i < studenti.length; i++){
    console.log(studenti[i].nome + "--" + studenti[i].cognome);
 }
 /* Dare la possibilità all’utente attraverso 3 prompt di
 aggiungere un nuovo oggetto studente inserendo
 nell’ordine: nome, cognome e età.*/
+ var nome;
+ nome = prompt("Inserisci il nome del nuovo studente");
+ var cognome;
+ cognome = prompt("Inserisci il cognome del nuovo studente");
+ var eta;
+ eta = parseInt(prompt("Inserisci l'età del nuovo studente"));
+studenti[3] = {
+  nome:nome,
+  cognome:cognome,
+  eta:eta
+}
+console.log(studenti);
