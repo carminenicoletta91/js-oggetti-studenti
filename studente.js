cf0c1f38c47796d04ed19ab28728e967ce2a4efa
carminenicoletta91 <carminenicolettakr91@gmail.com>
2019-07-10 16:27:37 +0200

    --completata richiesta 1 e richiesta 2 --

---
+++ studente.js
@@ -0,0 +1,43 @@
+/*Creare un oggetto che descriva uno studente con le seguenti proprietà:
+nome, cognome e età. Stampare a schermo attraverso
+il for in tutte le proprietà*/
+var studente = {
+  nome :"mario",
+  cognome : "rossi",
+  eta : 20
+};
+for (var x in studente) {
+ console.log(x + ":" + studente[x]);
+}
+/*Creare un array di oggetti di studenti.
+Ciclare su tutti gli studenti e stampare per ognuno nome e cognome*/
+var studenti =[
+  {
+    label:"studente1",
+    nome:"mario",
+    cognome:"rossi",
+    eta:20
+  },
+  {
+    label:"studente2",
+    nome:"filippo",
+    cognome:"inzaghi",
+    eta:40
+  },
+  {
+    label:"studente3",
+    nome:"cesare",
+    cognome:"cremonini",
+    eta:35
+  }
+
+];
+for(var i = 0; i < studenti.length; i++){
+   console.log(studenti[i].nome + "--" + studenti[i].cognome);
+}
+/* Dare la possibilità all’utente attraverso 3 prompt di
+aggiungere un nuovo oggetto studente inserendo
+nell’ordine: nome, cognome e età.*/
+var nomeut = prompt("Inserisci il nome del nuovo utente");
+studenti.nome.push({nomeut});
+console.log(studenti);
